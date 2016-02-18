c157371651706bac73a52d6ffa4f4e6405f89332
Anna Lawley and Alissa Paluska <alissa.paluska@gmail.com>
2016-02-18 10:30:28 -0800

    add phoneNumber to Contact

--- src/main/java/Phone.java
+++ src/main/java/Phone.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class Phone {
   private String mType;
   private String mAreaCode;
+  private String mPhoneNumber;
 
-  public Phone(String type, String areaCode) {
+  public Phone(String type, String areaCode, String number) {
     mType = type;
     mAreaCode = areaCode;
+    mPhoneNumber = number;
   }
 
   public String getType() {
@@ -15,5 +17,9 @@ public class Phone {
 
   public String getAreaCode() {
     return mAreaCode;
+  }
+
+  public String getPhoneNumber() {
+    return mPhoneNumber;
   }
 }
--- src/test/java/PhoneTest.java
+++ src/test/java/PhoneTest.java
@@ -7,19 +7,25 @@ public class PhoneTest {
 
   @Test
   public void phone_instantiatesCorrectly() {
-    Phone testPhone = new Phone("cell", "503");
+    Phone testPhone = new Phone("cell", "503", "555-5555");
     assertEquals(true, testPhone instanceof Phone);
   }
 
   @Test
   public void getType_instantiatesCorrectly_cell() {
-    Phone testPhone = new Phone("cell", "503");
+    Phone testPhone = new Phone("cell", "503", "555-5555");
     assertEquals("cell", testPhone.getType());
   }
 
   @Test
   public void getAreaCode_returnsAreaCode_503() {
-    Phone testPhone = new Phone("cell", "503");
+    Phone testPhone = new Phone("cell", "503", "555-5555");
     assertEquals("503", testPhone.getAreaCode());
+  }
+
+  @Test
+  public void getPhoneNumber_returnsPhoneNumber_555_5555() {
+    Phone testPhone = new Phone("cell", "503", "555-5555");
+    assertEquals("555-5555", testPhone.getPhoneNumber());
   }
 }
